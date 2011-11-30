6320119243418db13bb25336f99f680ff45f91a2
Fredrik Nannestad <fredrik.nannestad@gmail.com>
2011-11-30 16:21:59 +0100

    Absorption complete

--- GlossyVolume.cpp
+++ GlossyVolume.cpp
@@ -18,6 +18,34 @@ float3 GlossyVolume::shade(const Ray& r, HitInfo& hit, bool emit) const
   // Compute the specular part of the glossy shader and attenuate it
   // by the transmittance of the material if the ray is inside (as in
   // the volume shader).
-
-  return Volume::shade(r, hit, emit);
+    
+    float3 rho_s = get_specular(hit);
+    float s = get_shininess(hit);
+    
+    float3 phong = make_float3(0.0f);
+    
+    for (int i = 0; i < lights.size(); i++) {
+        float3 dir;
+        float3 L;
+        
+        if (lights[i]->sample(hit.position, dir, L)) {
+            float value = optix::dot(hit.shading_normal, dir);
+            if (value > 0) {
+                int n = (int)s;
+                //                int a = int_pow(optix::dot(-r.direction,optix::reflect(-dir, hit.shading_normal)), n);
+                float3 V = optix::reflect(-dir, hit.shading_normal);
+                float a = optix::dot(-r.direction, V);
+                if (a < 0.0) {
+                    a = 0;
+                } else {
+                    a = int_pow(a , n);
+                }
+                
+                phong += L * rho_s * a;
+            }
+        }
+    }
+        
+    return Volume::shade(r, hit, emit) + 0.1 * phong;
+ ;
 }
--- Volume.cpp
+++ Volume.cpp
@@ -10,10 +10,15 @@ using namespace optix;
 
 float3 Volume::shade(const Ray& r, HitInfo& hit, bool emit) const
 {
+    
   // If inside the volume, Find the direct transmission through the volume by using
   // the transmittance to modify the result from the Transparent shader.
-
-  return Transparent::shade(r, hit, emit);
+    bool inside = optix::dot(r.direction, hit.shading_normal) > 0;
+    if (inside) {
+        return Transparent::shade(r, hit, emit) * get_transmittance(hit);
+    } else {
+        return Transparent::shade(r, hit, emit);
+    }
 }
 
 float3 Volume::get_transmittance(const HitInfo& hit) const
@@ -24,6 +29,8 @@ float3 Volume::get_transmittance(const HitInfo& hit) const
     // Diffuse reflectance rho_d does not make sense for a specular material, so we can use 
     // this material property as an absorption coefficient. Since absorption has an effect
     // opposite that of reflection, using 1/rho_d-1 makes it more intuitive for the user.
+      float3 absCoefficient = 1 / optix::make_float3(hit.material->diffuse[0], hit.material->diffuse[1], hit.material->diffuse[2]) - 1;
+      return expf(-absCoefficient * hit.dist);
   }
   return make_float3(1.0f);
 }
